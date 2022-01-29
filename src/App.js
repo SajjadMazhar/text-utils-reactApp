64f184bd631f6648fbf1dc00da5297f961272707
Sajjad <sajjadmazharjr@gmail.com>
2022-01-29 07:35:45 +0530

    routing implemented

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import { useState } from 'react';
 import './App.css';
 import Alert from './components/Alert';
-// import About from './components/About';
+import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
   const [mode, setMode] = useState("light")
   const [alert, setAlert] = useState(null)
 
-  const showAlert = (message, type)=>{
+  const showAlert = (message, type) => {
     setAlert({
       message,
       type
@@ -19,25 +20,29 @@ function App() {
       setAlert(null)
     }, 3000);
   }
-  const toggleMode = ()=>{
-    if(mode==="light"){
+  const toggleMode = () => {
+    if (mode === "light") {
       setMode("dark")
       document.body.style.backgroundColor = "#1b1b31"
       showAlert("Dark mode has been enabled!", "success")
-    }else{
+    } else {
       setMode("light")
       document.body.style.backgroundColor = "white"
       showAlert("Light mode has been enabled!", "success")
     }
   }
   return (
-    <>  
-      <Navbar title="TextUtils" aboutText="About us" mode={mode} toggle = {toggleMode}/>
-      <Alert alert={alert}/>
-      <div className='container my-3'>
-        <TextForm heading="Enter the text to analyze" mode={mode} showAlert={showAlert}/>
-        {/* <About/> */}
-      </div>
+    <>
+      <Router>
+        <Navbar title="TextUtils" aboutText="About us" mode={mode} toggle={toggleMode} />
+        <Alert alert={alert} />
+        <div className='container my-3'>
+          <Routes>
+            <Route exact path="/about" element={<About/>}/>
+            <Route exact path="/" element={<TextForm heading="Enter the text to analyze" mode={mode} showAlert={showAlert} />} />
+          </Routes>
+        </div>
+      </Router>
     </>
   );
 }
